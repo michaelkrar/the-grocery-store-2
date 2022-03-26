4999ed38a2c6c6b78252d054f209c90a2039123e
michaelkrar <michael.krar1@gmail.com>
2022-03-25 20:53:43 -0400

    Add tile.java constructor Add tile thread Add tileInit and tilePeriodic

--- tile.java
+++ tile.java
@@ -1,3 +1,28 @@
-public class tile {
-    
+import java.util.ArrayList;
+
+public class tile implements Runnable{
+    ArrayList<person> people;
+    boolean isActive;
+    public tile(boolean isActive){
+        this.isActive = isActive;
+        tileInit();
+    }
+    public void tileInit(){
+        new Thread(this).start();
+    }
+    public void tilePeriodic(){
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+     
+    }
+    public void run(){
+        do {
+            this.tilePeriodic();
+            System.out.println("success");
+        }while(isActive);
+    }
 }
